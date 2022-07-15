00c29bf27fe2f16c6ad746433db4187f9b8ea09e
biradarajay1904 <biradarajay04@gmail.com>
2022-07-15 12:20:27 +0530

    added: start position

---
+++ Uc1.java
@@ -0,0 +1,16 @@
+package com.Snake.ladder;
+/**
+* ajay biradar
+
+
+*/
+
+public class Uc1 {
+	 static  int START_POSITION=0;
+
+	    public static void main(String[] args) {
+
+	        System.out.println("Welcome to Snake and ladder Game");
+	    }
+
+}
